fe83c2690ebb97e661ef84c6d93092cfcba17f85
chetanchaudhari789 <123301996+chetanchaudhari789@users.noreply.github.com>
2023-02-17 14:03:02 +0530

    Initial

---
+++ calculator.js
@@ -0,0 +1,31 @@
+(function(){
+
+    let screen = document.querySelector('.screen');
+    let buttons = document.querySelectorAll('.btn');
+    let clear = document.querySelector('.btn-clear');
+    let equal = document.querySelector('.btn-equal');
+
+    buttons.forEach(function(button){
+      button.addEventListener('click',function(e) {
+        let value = e.target.dataset.num;
+        console.log(value);
+        screen.value += value;
+      })
+    });
+
+    equal.addEventListener('click', function(e){
+      if(screen.value === ''){
+        screen.value = "";
+      }
+      else{
+        let answer = eval(eval(screen.value.split('u')[0]));
+        screen.value = answer;
+      }
+    })
+
+    clear.addEventListener('click', function(e){
+       screen.value = "";
+    })
+
+
+  })();
